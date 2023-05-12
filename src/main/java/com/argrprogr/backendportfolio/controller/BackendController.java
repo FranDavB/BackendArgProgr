2931292032eae052b1b396cdaea2dbc8d3ba13e9
FranDavB <francodbaretta@gmail.com>
2023-05-12 20:19:13 -0300

    BackendArgProgr-1.0

--- src/main/java/com/argrprogr/backendportfolio/controller/BackendController.java
+++ src/main/java/com/argrprogr/backendportfolio/controller/BackendController.java
@@ -12,26 +12,40 @@ import com.argrprogr.backendportfolio.model.Skill;
 import com.argrprogr.backendportfolio.services.AboutService;
 import com.argrprogr.backendportfolio.services.ExperienceService;
 import com.argrprogr.backendportfolio.services.FormationService;
+import com.argrprogr.backendportfolio.services.IStorageService;
 import com.argrprogr.backendportfolio.services.ProjectService;
 import com.argrprogr.backendportfolio.services.SkillService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  *
  * @author franc
  */
 @RestController
+@AllArgsConstructor
+
 public class BackendController {
     @Autowired
-    private ExperienceService expServ;
+    private final ExperienceService expServ;
    
     
     @GetMapping("/api/traer/experiencia")
@@ -75,7 +89,7 @@ public class BackendController {
     // About
     
     @Autowired
-    private AboutService abService;
+    private final AboutService abService;
     
     
     @GetMapping("/api/traer/acerca-de")
@@ -111,7 +125,7 @@ public class BackendController {
     //Formation
     
     @Autowired
-    private FormationService formationService;
+    private final FormationService formationService;
    
     
     @GetMapping("/api/traer/formacion")
@@ -153,7 +167,7 @@ public class BackendController {
     }
 
     @Autowired
-    private ProjectService projServ;
+    private final ProjectService projServ;
    
     
     @GetMapping("/api/traer/proyecto")
@@ -196,7 +210,7 @@ public class BackendController {
     //Skill
     
     @Autowired
-    private SkillService skillServ;
+    private final SkillService skillServ;
    
     
     @GetMapping("/api/traer/habilidad")
@@ -234,4 +248,33 @@ public class BackendController {
         return editSkill;
     }    
     
+    private final IStorageService storageService;  
+    private final HttpServletRequest request;
+    
+    
+    @PostMapping ("/subir/archivo")
+    public Map<String, String> uploadFile (@RequestParam("file") MultipartFile multipartFile) {
+        String path = storageService.store(multipartFile);
+        String host = request.getRequestURL().toString().replace(request.getRequestURI(), "");
+        String url = ServletUriComponentsBuilder
+                .fromHttpUrl(host)
+                .path("/obtener/archivo/")
+                .path(path)
+                .toUriString();
+        return Map.of("url",url);
+    }
+    
+    @GetMapping ("obtener/archivo/{nombreArchivo}")
+    public ResponseEntity <Resource> getFile(@PathVariable String nombreArchivo) throws IOException {
+        Resource file = storageService.loadAsResource(nombreArchivo);
+        String contentType = Files.probeContentType(file.getFile().toPath());
+        
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_TYPE, contentType)
+                .body(file);
+    }
+
 }
+    
+
